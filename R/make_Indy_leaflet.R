b7403ebb3689d5d02eb03e50a0d21a69825583f6
Dan Riggins <daniel.riggins@protonmail.com>
2021-02-15 18:07:15 -0500

    Adding zoom adjustment to make_Indy_map()

--- R/make_Indy_leaflet.R
+++ R/make_Indy_leaflet.R
@@ -6,9 +6,9 @@
 #' @export
 #'
 #' @examples
-make_Indy_leaflet <- function(df = NULL) {
+make_Indy_leaflet <- function(df = NULL, zoom = 10) {
     leaflet::leaflet(df) %>%
         leaflet::addProviderTiles("CartoDB") %>%
-        leaflet::setView(lng = -86.158, lat = 39.768, zoom =9) %>%
+        leaflet::setView(lng = -86.158, lat = 39.768, zoom = zoom) %>%
         leaflet.extras::addFullscreenControl()
 }
