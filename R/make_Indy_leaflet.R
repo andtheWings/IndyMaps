f37b8a3b4f65759b836f66500f0705fb27d082e4
Dan Riggins <daniel.riggins@protonmail.com>
2021-02-15 11:05:51 -0500

    Adding data argument to make_Indy_leaflet

--- R/make_Indy_leaflet.R
+++ R/make_Indy_leaflet.R
@@ -4,8 +4,8 @@
 #' @export
 #'
 #' @examples
-make_Indy_leaflet <- function() {
-    leaflet::leaflet() %>%
+make_Indy_leaflet <- function(df) {
+    leaflet::leaflet(df) %>%
         leaflet::addProviderTiles("CartoDB") %>%
         leaflet::setView(lng = -86.158, lat = 39.768, zoom =9) %>%
         leaflet.extras::addFullscreenControl()
