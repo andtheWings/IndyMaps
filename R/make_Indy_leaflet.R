3b169134e53dd597f55a06536f32454c360edec8
Dan Riggins <daniel.riggins@protonmail.com>
2021-02-15 10:37:04 -0500

    Change leaflet package to leaflet.extras

--- R/make_Indy_leaflet.R
+++ R/make_Indy_leaflet.R
@@ -2,5 +2,5 @@ make_Indy_leaflet <- function() {
     leaflet::leaflet() %>%
         leaflet::addProviderTiles("CartoDB") %>%
         leaflet::setView(lng = -86.158, lat = 39.768, zoom =9) %>%
-        leaflet::addFullscreenControl()
+        leaflet.extras::addFullscreenControl()
 }
