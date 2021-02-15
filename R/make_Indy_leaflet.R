5f5a6eac85b551b20e21c5df71c61bde770b6468
Dan Riggins <daniel.riggins@protonmail.com>
2021-02-15 10:39:14 -0500

    Documenting make_Indy_leaflet

--- R/make_Indy_leaflet.R
+++ R/make_Indy_leaflet.R
@@ -1,3 +1,9 @@
+#' Make a leaflet basemap of Indianapolis, IN
+#'
+#' @return A leaflet htmlwidget
+#' @export
+#'
+#' @examples
 make_Indy_leaflet <- function() {
     leaflet::leaflet() %>%
         leaflet::addProviderTiles("CartoDB") %>%
