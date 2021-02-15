8fc2cec90261f963648ad82a1eb9132762b59d07
Dan Riggins <daniel.riggins@protonmail.com>
2021-02-15 16:10:33 -0500

    Setting default value of df in make_Indy_leaflet() to NULL

--- R/make_Indy_leaflet.R
+++ R/make_Indy_leaflet.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 #' @examples
-make_Indy_leaflet <- function(df) {
+make_Indy_leaflet <- function(df = NULL) {
     leaflet::leaflet(df) %>%
         leaflet::addProviderTiles("CartoDB") %>%
         leaflet::setView(lng = -86.158, lat = 39.768, zoom =9) %>%
